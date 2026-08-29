671c94e69da4cec4f5476555d25ecaa11a4464b5
EseTheGreat <317424051+Ese-G-Oduware@users.noreply.github.com>
2026-08-29 11:38:18 -0500

    Add files via upload

---
+++ test_code/RISC_V_Ex_code.cpp
@@ -0,0 +1,24 @@
+//The code below was compiled and loaded into instruction memory.
+//Credits to bit-spinner.com for providing the program. 
+
+int fibonacci(int n) {
+    
+    if (n <= 0)
+        return;
+
+    int a = 0, b = 1, c, i;
+
+    for (i = 1; i < n; i++) {
+        c = a + b; // Calculate the next term
+        a = b;     // Update a to the previous term
+        b = c;     // Update b to the current term
+    }
+
+    return a;
+}
+
+int main(void) {
+
+    int fib = fibonacci(5);
+    return 0;        
+}
